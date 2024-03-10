86544d601c5a40aeb7304728a37af8d06e75ec55
pwansbypass <95245101+pwansbypass@users.noreply.github.com>
2024-03-11 04:23:26 +0800

    Update Vape.lua

--- Vape.lua
+++ Vape.lua
@@ -144,15 +144,31 @@ function lib:Window(text, preset, closebind)
     UserInputService.InputBegan:Connect(
         function(io, p)
             if io.KeyCode == CloseBind then
-		uitoggled = not uitoggled
-		   if uitoggled then
-		      ui.Enabled = false
-		   else
-		      ui.Enabled = true
-		end	
+				uitoggled = not uitoggled
+                if uitoggled then
+                    Main:TweenSize(
+                        UDim2.new(0, 0, 0, 0), 
+                        Enum.EasingDirection.Out, 
+                        Enum.EasingStyle.Quart, 
+                        .6, 
+                        true, 
+                        function()
+                            ui.Enabled = false
+                        end
+                    )
+                else
+                    ui.Enabled = true
+                    Main:TweenSize(
+                        UDim2.new(0, 560, 0, 319),
+                        Enum.EasingDirection.Out,
+                        Enum.EasingStyle.Quart,
+                        .6,
+                        true
+                    )
+                end
             end
         end
-    end)
+    )
 
     TabFolder.Name = "TabFolder"
     TabFolder.Parent = Main
